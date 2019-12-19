8b0f42149846afc37bceac414bcf6442a51985c5
Mikaël MAROLLEAU-POIRIER <mikael.mp@icloud.com>
2019-12-19 10:25:26 +0100

    ajout salon

--- src/Controller/EtageController.php
+++ src/Controller/EtageController.php
@@ -40,4 +40,36 @@ class EtageController extends AbstractController
     {
         return $this->render('loupe.html.twig');
     }
+
+    /**
+     * @Route("game/salon", name="game_salon", methods={"GET"})
+     */
+    public function salon(): Response
+    {
+        return $this->render('salon.html.twig');
+    }
+
+    /**
+     * @Route("game/cheminee", name="game_cheminee", methods={"GET"})
+     */
+    public function cheminee(): Response
+    {
+        return $this->render('cheminee.html.twig');
+    }
+
+        /**
+     * @Route("game/wc", name="game_wc", methods={"GET"})
+     */
+    public function wc(): Response
+    {
+        return $this->render('wc.html.twig');
+    }
+
+        /**
+     * @Route("game/cuisine", name="game_cuisine", methods={"GET"})
+     */
+    public function cuisine(): Response
+    {
+        return $this->render('cuisine.html.twig');
+    }
 }
